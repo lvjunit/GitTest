d53af1709c0ab0b679c2e8f84dd833e64fb80d48
lvjun <lvjun@guahao.com>
2018-09-28 11:05:40 +0800

    Commit Message测试Git

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("测试一下");
+        System.out.println("测试一下2");
     }
 }
